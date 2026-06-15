ace1226ec8c7a48e3c46e24905ce57371f35f6fb
MorphoMenelaus <40551149+MorphoMenelaus@users.noreply.github.com>
2026-06-14 21:16:54 -0400

    Added backend for a movie database query based on Plex Media DB
    Added backend for a movie database query based on Plex Media DB

--- controllers/userLogsController.js
+++ controllers/userLogsController.js
@@ -208,7 +208,90 @@ const logSimpleClock = async (req, res) => {
 		// Crucial: Always release the connection back to the pool
 		if (conn) conn.release();
 	}
-
 }
 
-module.exports = { getUserLogs, addUserLogs, getClockLog, logSimpleClock };
+/**
+ * Retrieves logs all movies, if authenticated via an access token.
+ * 
+ * @name getMovieData
+ * @route {GET} /api/userlogs/movies
+ * @access Restricted (Requires Bearer Token)
+ * @auth Requires JWT access token in the Authorization header.
+ * 
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Express next middleware function
+ * @returns {Promise<void>}
+ */
+const getMovieData = async (req, res) => {
+
+	const resultLimit = req.query.limit || process.env.LIST_LIMIT_DEFAULT;
+	const resultOffset = req.query.offset || 0;
+	// const sortBy = req.query.sort || 'title';
+
+	// const allowedColumns = ['title', 'rating', 'year', 'tags_director', 'tags_genre'];
+	// // const allowedDirections = ['ASC', 'DESC'];
+
+	// const sortBy = allowedColumns.includes(req.query.sort)
+	// 	? req.query.sort
+	// 	: 'title';
+
+	// const direction = allowedDirections.includes(req.query.dir?.toUpperCase())
+	// 	? req.query.dir.toUpperCase()
+	// 	: 'DESC';
+
+	// Get a connection from the pool
+	const conn = await pool.getConnection();
+
+	try {
+		// Clear snapshot cache to prevent stale data (forces a fresh read)
+		await conn.query("COMMIT");
+
+		const columns = [
+			"title",
+			"original_title",
+			"tagline",
+			"summary",
+			"studio",
+			"rating",
+			"content_rating",
+			"duration",
+			"tags_genre",
+			"tags_director",
+			"tags_writer",
+			"tags_star",
+			"year",
+			"tags_country",
+			"audience_rating",
+			"slug",
+		]
+
+		// Execute the query
+		// const query = `SELECT ${columns} FROM metadata_items ORDER BY 
+		// CASE WHEN @sortBy = 'title' THEN title END, 
+		// CASE WHEN @sortBy = 'rating' THEN rating END, 
+		// CASE WHEN @sortBy = 'year' THEN year END`;
+		const query = `SELECT ${columns} FROM metadata_items ORDER BY importIndex ASC LIMIT ? OFFSET ?`;
+		const rows = await conn.execute(query, [Number(resultLimit), Number(resultOffset)]);
+
+		// Send the JSON response
+		res.status(200).json({
+			code: 200,
+			message: "User logs query success",
+			success: true,
+			movies: rows,
+		});
+
+	} catch (error) {
+		res.status(500).json({
+			code: 500,
+			message: "Database query failed",
+			success: false,
+		});
+	} finally {
+		// Crucial: Always release the connection back to the pool
+		if (conn) conn.release();
+	}
+}
+
+module.exports = { getUserLogs, addUserLogs, getClockLog, logSimpleClock, getMovieData };
--- routes/userLogsRoutes.js
+++ routes/userLogsRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authenticateToken = require("../middleware/authenticationHandler");
-const { getUserLogs, addUserLogs, getClockLog, logSimpleClock } = require("../controllers/userLogsController");
+const { getUserLogs, addUserLogs, getClockLog, logSimpleClock, getMovieData } = require("../controllers/userLogsController");
 
 router.route("/").get(authenticateToken, getUserLogs);
 
@@ -10,5 +10,7 @@ router.route("/").post(authenticateToken, addUserLogs);
 router.route("/clock/log").get(authenticateToken, getClockLog);
 
 router.route("/clock/").post(authenticateToken, logSimpleClock);
+
+router.route("/movies/").get(getMovieData);
 
 module.exports = router;
