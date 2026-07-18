fb07aa9cc4899e83a8a488f086c39b43aa0da1f9
MorphoMenelaus <40551149+MorphoMenelaus@users.noreply.github.com>
2026-07-18 10:36:34 -0400

    Added a checkToken endpoint

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -117,7 +117,6 @@ const login = async (req, res) => {
 	}
 }
 
-
 /**
  * Refreshes an expired authentication token if the refresh token is valid and is no more than a week old.
  * 
@@ -256,6 +255,63 @@ const logout = async (req, res) => {
 		// Crucial: Always release the connection back to the pool
 		if (conn) conn.release();
 	}
-};
+}
 
-module.exports = { login, refresh, logout };
+/**
+ * Check if the refresh token is valid and is no more than a week old.
+ * 
+ * @name checkToken
+ * @route {POST} /api/auth/checkToken
+ * @access public
+ * 
+ * @param {Object} req - Express request object
+ * @param {Object} res - If there is no 500 internal error, returns code 200 with a Boolean for tokenValid.
+ * @returns {Promise<void>}
+ */
+const checkToken = async (req, res) => {
+	const { accessToken, refreshToken } = req.body;
+
+	// Get a connection from the pool
+	let conn = await pool.getConnection();
+
+	try {
+
+		// Match active refresh token and get user record
+		const users = await conn.query(`SELECT * FROM users WHERE refreshToken = '${refreshToken}'`);
+		let singleUser = users[0];
+		let userName = singleUser?.userName;
+
+		if (singleUser?.refreshToken !== refreshToken) {
+			res.status(200).json({
+				code: 200,
+				tokenValid: false,
+				success: false,
+			});
+		}
+
+		jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, userName) => {
+			if (err) return res.status(200).json({
+				code: 200,
+				tokenValid: false,
+				success: false,
+			});
+		});
+
+		res.status(200).json({
+			code: 200,
+			tokenValid: true,
+			success: true,
+		});
+
+	} catch {
+		res.status(500).json({
+			code: 500,
+			message: "Internal Server Error",
+			success: false,
+		});
+	} finally {
+		if (conn) conn.release();
+	}
+}
+
+module.exports = { login, refresh, logout, checkToken };
